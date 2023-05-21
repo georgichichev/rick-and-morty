d92a20ed2bd778b073aa0f1ee6fc6dbb7e52ee24
Georgi <georgichichev15@gmail.com>
2023-05-21 14:59:53 +0300

    Add not found page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,13 @@
 import { useRoutes } from 'react-router-dom';
 
 export const AppRoutes = () => {
-  const commonRoutes = [{ path: '/', element: <div>Welcome to the app</div> }];
+  const commonRoutes = [
+    {
+      path: '/',
+      element: <div>Welcome to the app</div>,
+    },
+    { path: '*', element: <div>Page not found...</div> },
+  ];
 
   const element = useRoutes(commonRoutes);
 
