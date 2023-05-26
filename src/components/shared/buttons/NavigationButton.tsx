724629040a18f9faf9531d081e478c231baf5100
Georgi <georgichichev15@gmail.com>
2023-05-26 12:28:21 +0300

    Change styles to navigation button

--- src/components/shared/buttons/NavigationButton.tsx
+++ src/components/shared/buttons/NavigationButton.tsx
@@ -1,4 +1,5 @@
-import { styled, theme } from '@chakra-ui/react';
+import { theme } from '@/theme';
+import { styled } from '@chakra-ui/react';
 import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
@@ -9,15 +10,26 @@ interface NavigationButtonProps {
 
 const NavButton = styled(NavLink, {
   baseStyle: {
-    color: theme.colors.orange[300],
-    _hover: { color: 'white' },
+    fontWeight: theme.fontWeights.bold,
+    fontSize: theme.fontSizes['lg'],
+    color: theme.colors.text.primary,
+    _hover: {
+      color: theme.colors.text.hover,
+      borderColor: theme.colors.text.hover,
+    },
     transition: 'ease-in-out 0.2s',
+    paddingBottom: theme.space[1],
   },
 });
 
 export const NavigationButton: FC<NavigationButtonProps> = ({ children, to }) => {
   return (
-    <NavButton to={to} style={({ isActive }) => ({ color: isActive ? 'yellow' : '' })}>
+    <NavButton
+      to={to}
+      style={({ isActive }) => ({
+        borderBottom: isActive ? `1px solid` : 'none',
+      })}
+    >
       {children}
     </NavButton>
   );
