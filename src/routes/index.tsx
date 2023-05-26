7b53ae2bbb597a6eca48e208a5697d67a47ea5b8
Georgi <georgichichev15@gmail.com>
2023-05-26 11:56:50 +0300

    Change main content layout structure
    + Add episodes and locations routes to app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,19 @@
+import { Header } from '@/components/common/header';
 import { Landing } from '@/features/landing';
+import { Box, Container, theme } from '@chakra-ui/react';
 import { Outlet, useRoutes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <div>App header</div>
-      <Outlet />
-    </>
+    <Box width="100%" height="100vh" bg="gray.800" color="gray.100">
+      <Container maxWidth="8xl" padding={theme.space[5]}>
+        <Header />
+        <Outlet />
+      </Container>
+    </Box>
   );
 };
+
 export const AppRoutes = () => {
   const commonRoutes = [
     {
@@ -20,6 +25,8 @@ export const AppRoutes = () => {
       element: <App />,
       children: [
         { path: 'characters', element: <div>Characters page</div> },
+        { path: 'episodes', element: <div>Episodes page</div> },
+        { path: 'locations', element: <div>Locations page</div> },
         { path: '', element: <div>Dashboard page</div> },
       ],
     },
